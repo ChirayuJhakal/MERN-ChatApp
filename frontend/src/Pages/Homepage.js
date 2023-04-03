05d26ba188715431c9688a05cdb40038d754234d
Chirayu Jhakal <jhakalchirayu@gmail.com>
2023-04-03 23:11:53 +0530

    Header n Chat Page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Container,
   Box,
@@ -11,12 +11,21 @@ import {
 } from '@chakra-ui/react';
 import Login from '../components/Authentication/Login';
 import Signup from '../components/Authentication/Signup';
+import { useHistory } from 'react-router-dom';
 
 const Homepage = () => {
+  const history = useHistory();
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('userInfo'));
+
+    if (user) {
+      history.push('/chats');
+    }
+  }, [history]);
   return (
     <Container maxW='xl' centerContent>
       <Box
-        d='flex'
+        display='flex'
         justifyContent='center'
         p={3}
         bg={'white'}
