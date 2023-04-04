8928c4ec56eeaeb01685f03f457a736a2e0e9dbd
Chirayu Jhakal <jhakalchirayu@gmail.com>
2023-04-04 13:07:41 +0530

    Complete Create Group

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { AddIcon } from '@chakra-ui/icons';
 import ChatLoading from './ChatLoading';
 import { getSender } from '../config/ChatLogics';
+import GroupChatModal from './misc/GroupChatModal';
 
 const MyChats = () => {
   const [loggedUser, setLoggedUser] = useState();
@@ -63,13 +64,15 @@ const MyChats = () => {
       >
         {' '}
         My Chats
-        <Button
-          display='flex'
-          fontSize={{ base: '17px', md: '10px', lg: '17px' }}
-          rightIcon={<AddIcon />}
-        >
-          New Group Chat
-        </Button>
+        <GroupChatModal>
+          <Button
+            display='flex'
+            fontSize={{ base: '17px', md: '10px', lg: '17px' }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        </GroupChatModal>
       </Box>
 
       <Box
